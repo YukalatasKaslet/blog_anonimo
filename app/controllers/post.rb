70cf72341c70509a7646ba8a7b3786aedeb2aafd
codeamac01 <codeamac01@gmail.com>
2016-08-01 13:36:37 -0500

    se añadieron rutas CRUD, falta delete

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,12 +1,34 @@
-post '/post' do
-  @post = Post.create(params[:post])
-  erb :show
+
+
+
+get '/post/:id/edit' do
+  #edit
+  @post = Post.find(params[:id]) 
+  erb :edit
 end
 
 
 get '/post/:id' do
+  #read
   @post = Post.find(params[:id])  
   erb :post 
 end
 
 
+put '/post/:id' do
+  @post = Post.find(params[:id])
+  @post.update(params[:post])
+  erb :post
+
+end
+
+post '/post' do
+  #create
+  @post = Post.create(params[:post])
+  erb :post
+end
+
+get '/show' do
+  @post = Post.all
+  erb :show
+end
